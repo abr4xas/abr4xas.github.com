72adfcccc09f2a6a4ac8e697a9de2c93f4151a82
abr4xas <bullgram@gmail.com>
2013-08-21 08:50:48 -0430

    new file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>abr4xas.org</title>
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="css/base.css"/>
+</head>
+	<body>
+        <div class="container">		
+			<img height="100%" width="100%" style="display:block; margin:0 auto 0 auto; " title="El blog de abr4xas" src="http://abr4xas.org/themes/simpler/css/img/abr4xas468.png"></img>
+			<br>
+			<h1>Hello World!!</h1>
+        </div>		
+	</body>
+</html>
